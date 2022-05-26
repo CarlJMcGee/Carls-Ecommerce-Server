6a6dde234cbb0df36495720683f10cce00f349be
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-05-25 21:08:01 -0500

    fixed get all, added get id, post, put

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,22 +5,49 @@ const { Category, Product } = require("../../models");
 
 router.get("/", async (req, res) => {
   // find all categories
-  const categories = await Product.findAll({});
+  const categories = await Category.findAll({
+    // be sure to include its associated Products
+    include: {
+      model: Product,
+      require: true,
+      attributes: ["product_name"],
+    },
+    order: [["category_name", "ASC"]],
+  });
+  res.json(categories);
+});
+
+router.get("/:id", async (req, res) => {
+  // find one category by its `id` value
+  const categories = await Category.findAll({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+      require: true,
+      attributes: ["product_name"],
+    },
+  });
   // be sure to include its associated Products
   res.json(categories);
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
-
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  console.log(req.body);
+  const newCategory = await Category.create(req.body);
+  res.status(201).send("added new category");
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  const category = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.status(202).send("category name updated");
 });
 
 router.delete("/:id", (req, res) => {
